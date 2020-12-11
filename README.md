29e67a8bf45233368f0d10e7fb97cca46ada711d
Wes8830 <wes.vesprini@gmail.com>
2020-12-10 19:19:44 -0500

    Got My Array Parsed

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Web-and-Flask
 Web and Flask Framework
+
+We're building a Framework using Flask which can repurposed for a variety of content sets.
+
+#first will be implementing this as a Roadmap Manager where we'll be rendering a Roadmap from a DB file
+#Then adding logic to update that roadmap
--- application.py
+++ application.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
+#Help with Importing SQL and working with it: https://www.digitalocean.com/community/tutorials/how-to-use-the-sqlite3-module-in-python-3
+
+rows = ['Asset Class', 'Portfolio Monitoring', 'Trade Simulation', 'Core Capabilities', 'OMS Connectivity']
+
 @app.route("/")
 def load_categories():
-    #return 'Hello, World!'
-    #categories = db.execute("SELECT * FROM categories")
-    return render_template("index.html")#, category=categories)
+    #connection = sqlite3.connect("categories.db")
+    #db = connection.cursor()
+    #rows = db.execute("SELECT * FROM categories")
+    return render_template("index.html", rows=rows)
+
+@app.route("/about")
+def load_about():
+    return render_template("about.html")
+
+#@aprows
